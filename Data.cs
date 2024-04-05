299a3a4ba502bb2f9c56048b77dcd99c6a292c2e
boryaxray <157354076+boryaxray@users.noreply.github.com>
2024-04-05 13:40:35 +0500

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kr2
+{
+    class Data
+    {
+        // Поле DataTime data.
+        DateTime date;
+        // установить аданную дату
+        public Data(int year, int month, int day)
+        {
+            date = new DateTime(year, month, day);
+        }
+        // дату 1.04.2024
+        public Data()
+        {
+            date = new DateTime(2024, 4, 1);
+        }
+        //вычислить дату предыдущего дня;
+        public DateTime PrevDay()
+        {
+            return date.AddDays(-1);
+        }
+        //вычислить дату следующего дня;
+        public DateTime NextDay()
+        {
+            return date.Add(TimeSpan.FromDays(1));
+        }
+        //определить сколько дней осталось до конца месяца
+        public int DaysLeft()
+        {
+            return DateTime.DaysInMonth(date.Year, date.Month) - date.Day;
+        }
+        //установить или получить значение поле класса (доступно для чтения и записи)
+        public DateTime Date
+        {
+            set { date = value; }
+            get { return date; }
+        }
+        //позволяющее определить год высокосным (доступно только для чтения)
+        public bool IsLeap
+        {
+            get { return DateTime.IsLeapYear(date.Year); }
+        }
+        // Индексатор, позволяющий определить дату i-того по счету дня относительно установленной даты 
+        // (при отрицательных значениях индекса отсчет ведется в обратном порядке). 
+        public DateTime this[int index]
+        {
+            get{ return date.AddDays(index);}
+        }
+        //Перегрузку:операции !: возвращает значение true, если установленная дата не является последним днем месяца, иначе false;
+        public static bool operator !(Data a)
+        {
+            return a.date.Day != DateTime.DaysInMonth(a.date.Year, a.date.Month);
+        }
+        // констант true и false: обращение к экземпляру класса дает значение true,
+        // если установленная дата является началом года, иначе false;
+        public static bool operator true(Data a)
+        {
+            return a.date.Day == 1 && a.date.Month == 1;
+        }
+        public static bool operator false(Data a)
+        {
+            return a.date.Day != 1 || a.date.Month != 1;
+        }
+        // операции &: возвращает значение true, если поля двух объектов равны, иначе false;
+        public static bool operator &(Data a, Data b)
+        {
+            return a.date.Equals(b.date);
+        }
+        public static explicit operator String(Data obj)
+        {
+            return " Год: " + obj.date.Year + " Месяц: " + obj.date.Month + " Число: " + obj.date.Day;
+        }
+        public static explicit operator Data(String str)
+        {
+            string[] str_arr = str.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Data obj = new Data(Convert.ToInt32(str_arr[0]), Convert.ToInt32(str_arr[1]), Convert.ToInt32(str_arr[2]));
+            return obj;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data my = new Data();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Текущая дата: {(string)my}");
+            my = new Data(2021, 7, 26);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Изменить дату на 26.7.2021: {(string)my}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Дата предыдущего дня: {my.PrevDay()}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Дата следующего дня: {my.NextDay()}");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Количество дней до конца месяца: { my.DaysLeft()}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Индексатор, позволяющий определить дату i-того по счету дня относительно установленной даты.Добавить к дате 10 дней: {my[10]}");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Является ли год выскокосным? {my.IsLeap}");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Перегрузка операции !: true, если дата не является последним днем месяца, иначе false: {!my}");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"Создается новый обьект DaateClass(2021, 7, 26)");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Data my1 = new Data(2021, 7, 26);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"Перегрузка операции &: true, если поля двух объектов равны, иначе false: {my1 & my}");
+            Console.ReadKey();
+        }
+    }
+}
